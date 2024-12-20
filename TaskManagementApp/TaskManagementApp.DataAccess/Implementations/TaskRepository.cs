25b965d2220f9e81b63b1a777246e2b580275507
borrison-s <boris.stojkoski@gmail.com>
2024-12-20 17:27:27 +0100

    Add IRepository interfaces and implementation TaskRepository

---
+++ TaskManagementApp/TaskManagementApp.DataAccess/Implementations/TaskRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementApp.DataAccess.Interfaces;
+using TaskManagementApp.Domain.Models;
+
+namespace TaskManagementApp.DataAccess.Implementations
+{
+    public class TaskRepository : IRepository<TaskModel>
+    {
+        private readonly TaskManagementDbContext _context;
+
+        public TaskRepository(TaskManagementDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public List<TaskModel> GetAll()
+        {
+            return _context
+                .Tasks
+                .Include(x => x.User) //join with table Users
+                .ToList();
+
+        }
+
+        public TaskModel GetById(int id)
+        {
+            return _context.Tasks
+                .Include(x => x.User)
+                .FirstOrDefault(x => x.Id == id);
+
+        }
+
+        public void Add(TaskModel entity)
+        {
+            _context.Tasks.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TaskModel entity)
+        {
+            _context.Tasks.Update(entity);
+            _context.SaveChanges();
+
+        }
+
+        public void Delete(TaskModel entity)
+        {
+            _context.Tasks.Remove(entity);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ TaskManagementApp/TaskManagementApp.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskManagementApp.Domain.Models;
+
+namespace TaskManagementApp.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        //CRUD
+        List<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
