09b32f49a3622655787dd07a4c2651c2a4fd3950
borrison-s <boris.stojkoski@gmail.com>
2024-12-20 17:56:44 +0100

    Add new project TaskManagementApp.Mappers for mapping Tasks to TaskDtos

---
+++ TaskManagementApp/TaskManagementApp.Mappers/Tasks/TaskMapper.cs
@@ -0,0 +1,21 @@
+﻿using TaskManagementApp.Domain.Models;
+using TaskManagementApp.Dtos.Tasks;
+
+namespace TaskManagementApp.Mappers.Tasks
+{
+    public static class TaskMapper
+    {
+        public static TaskDto ToTaskDto(this TaskModel task)
+        {
+            return new TaskDto
+            {
+                Title = task.Title,
+                Description = task.Description,
+                DuteDate = task.DueDate,
+                Priority = task.Priority,
+                Status = task.Status,
+                UserFullName = $"{task.User.FirstName} {task.User.LastName}"
+            };
+        } 
+    }
+}
