54f1205d4e7947fe6a61ee6efa0838da72728dd2
borrison-s <boris.stojkoski@gmail.com>
2024-12-23 13:48:27 +0100

    Add TaskController with GetAll method

---
+++ TaskManagementApp/TaskManagementApp/Controllers/TasksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementApp.Dtos.Tasks;
+using TaskManagementApp.Services.Interfaces;
+
+namespace TaskManagementApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly ITaskService _taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<TaskDto>> GetAll()
+        {
+            try
+            {
+                return Ok(_taskService.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured");
+            }
+        }
+    }
+}
